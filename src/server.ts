9502f52d5d66ac4a3981b4d5b6d55456fef3bcec
ctownsdin <townsdin@gmail.com>
2021-10-18 09:58:23 -0700

    add types for req and res

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,10 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import bodyParser from 'body-parser';
 import { filterImageFromURL, deleteLocalFiles } from './util/util';
 
 (async () => {
-    // Init the Express application
     const app = express();
-
-    // Set the network port
     const port = process.env.PORT || 8082;
-
-    // Use the body parser middleware for post requests
     app.use(bodyParser.json());
 
     // TODO:1 IMPLEMENT A RESTFUL ENDPOINT
@@ -29,11 +24,10 @@ import { filterImageFromURL, deleteLocalFiles } from './util/util';
 
     // Root Endpoint
     // Displays a simple message to the user
-    app.get('/', async (req, res) => {
+    app.get('/', async (req: Request, res: Response) => {
         res.send('try GET /filteredimage?image_url={{}}');
     });
 
-    // Start the Server
     app.listen(port, () => {
         console.log(`server running http://localhost:${port}`);
         console.log(`press CTRL+C to stop server`);
